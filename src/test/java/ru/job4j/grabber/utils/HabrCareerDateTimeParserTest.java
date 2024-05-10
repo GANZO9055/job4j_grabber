9b4edb493e14f4c043042ff11d7033cee3924e91
GANZO9055 <kosta.maslihov@gmail.com>
2024-05-10 10:49:24 +0300

    Преобразование даты [#289476]

---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    public void parseDateTimeString() {
+        String time = "2024-05-03T18:27:26";
+        HabrCareerDateTimeParser test = new HabrCareerDateTimeParser();
+
+        LocalDateTime result = test.parse(time);
+
+        assertThat(LocalDateTime.of(2024, 5, 3, 18, 27, 26)).isEqualTo(result);
+    }
+
+    @Test
+    public void test_parse_validDateTimeStringWithDifferentTimeZones() {
+        String dateTimeString = "2023-11-04T18:27:26";
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+
+        LocalDateTime result = parser.parse(dateTimeString);
+
+        assertThat(LocalDateTime.of(2023, 11, 4, 18, 27, 26)).isEqualTo(result);
+    }
+}
